9aac06c5d9f7f36de6b68de56c2eadc55b13bc42
Simon Liu <ysimonliu@gmail.com>
2013-03-18 16:33:00 +1000

    initial commit of source code including an example: HelloWorld

---
+++ com.yanjiliu.smarthome/src/elvin/ElvinConsumer.java
@@ -0,0 +1,38 @@
+package elvin;
+
+import org.avis.client.*;
+
+public class ElvinConsumer{
+
+    private Elvin elvin;
+
+public ElvinConsumer(String url){
+
+	try{
+
+	    elvin = new Elvin(url);
+	    Subscription sub = elvin.subscribe("NAME == 'test'"); 
+
+	    sub.addListener(new NotificationListener(){
+
+  	      public void notificationReceived(NotificationEvent event){
+
+	       //print out the notification
+               System.out.println(event.notification.get("NAME"));
+
+               }
+             });
+
+
+} catch (Exception e){
+
+    e.printStackTrace();
+}
+}
+
+ public static void main(String [] args){
+
+	ElvinConsumer me = new ElvinConsumer(args[0]);
+
+ } 
+}
---
+++ com.yanjiliu.smarthome/src/elvin/ElvinProducer.java
@@ -0,0 +1,47 @@
+package elvin;
+
+import org.avis.client.*;
+
+public class ElvinProducer{
+
+    private Elvin elvin;
+
+    public ElvinProducer(String server){
+
+	try{
+
+		elvin = new Elvin(server);
+		System.out.println("connected");
+
+	} catch (Exception e){
+
+    	e.printStackTrace();
+
+	}
+}
+
+
+public void produce(){
+
+	Notification not;
+
+	try{
+
+	    not = new Notification();
+	    not.set("NAME", "test");     
+	    elvin.send(not); //send the notification
+
+	} catch (Exception e){
+
+    		e.printStackTrace();
+	}
+}
+
+
+public static void main(String [] args){
+
+	ElvinProducer me = new ElvinProducer(args[0]);
+	me.produce();
+
+}
+}
